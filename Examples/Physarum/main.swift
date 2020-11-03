356add44bef7ef869cdd2b29afb104195216f491
Brad Larson <bradlarson@google.com>
2020-11-03 12:47:37 -0600

    Working through to argmax / argmin.

--- Examples/Physarum/main.swift
+++ Examples/Physarum/main.swift
@@ -16,9 +16,12 @@ import Foundation
 import ModelSupport
 import SwiftRT
 
-let stepCount = 5
-let gridSize = 512
-let particleCount = 1024
+//let stepCount = 5
+let stepCount = 1
+//let gridSize = 512
+let gridSize = 10
+//let particleCount = 1024
+let particleCount = 5
 let senseAngle = 0.20 * Float.pi
 let senseDistance: Float = 4.0
 let evaporationRate: Float = 0.95
@@ -26,7 +29,6 @@ let moveAngle = 0.1 * Float.pi
 let moveStep: Float = 2.0
 let channelSize = 1
 let captureImage = true
-
 let runOnCPU = false
 
 if runOnCPU { use(device: 0) }
@@ -36,7 +38,7 @@ var grid = [repeating(0, (gridSize, gridSize), type: Float.self),
 var positions = TensorR2<Float>(randomUniform: Shape2(particleCount, 2)) * Float(gridSize)
 var headings = TensorR1<Float>(randomUniform: particleCount) * 2.0 * Float.pi
 
-let gridShape = Shape2(gridSize, gridSize)
+let gridShape = repeating(array([Int32(gridSize), Int32(gridSize)], (1, 1, 2)), (particleCount, 3, 2))
 
 extension Tensor where TensorElement.Value: Numeric {
   func mask(condition: (Tensor<Shape, TensorElement>) -> Tensor<Shape, Bool>) -> Tensor<Shape, TensorElement> {
@@ -64,22 +66,37 @@ func step(phase: Int) {
   // TODO: This wrapping around negative values needs to be fixed.
   let sensingIndices = abs(TensorR3<Int32>(sensingPosition))
   // TODO: Add the following modulus to the above.
-  //  % (gridShape.expandingShape(at: 0).expandingShape(at: 0))
-  
+  //  % gridShape
+
   // TODO: Gather
   //  let sensedValues = currentGrid.expandingShape(at: 2)
   //    .dimensionGathering(atIndices: sensingIndices).squeezingShape(at: 2)
-  
-  /*
+
+  let sensedValues = array([-3.0, 0.0, 2.0,
+                            1.0, 1.0, 1.0,
+                            1.0, 2.0, 3.0,
+                            3.0, 2.0, 1.0,
+                            -1.0, -2.0, -3.0], (5, 3))
+
   // Move
-  let lowValues = sensedValues.argmin(squeezingAxis: -1)
-  let highValues = sensedValues.argmax(squeezingAxis: -1)
-  let middleMask = lowValues.mask { $0 .== 1 }
-  let middleDistribution = Tensor<Float>(randomUniform: [particleCount], on: device)
-  let randomTurn = middleDistribution.mask { $0 .< 0.1 } * Tensor<Float>(middleMask)
-  let turn = Tensor<Float>(highValues - 1) * Tensor<Float>(1 - middleMask) + randomTurn
-  headings += (turn * moveAngle)
-  positions += angleToVector(headings) * moveStep
+
+  // lowValues should be [0, 0-2, 0, 2, 2]
+  // highValues should be [2, 0-2, 2, 0, 0]
+  let lowValues = argmin(sensedValues)
+  let highValues = argmax(sensedValues)
+  
+  print("lowValues shape: \(lowValues.shape)")
+  print("lowValues: \(lowValues)")
+//  let middleMask = lowValues.mask { $0 .== 1 }
+  
+  let middleDistribution = TensorR1<Float>(randomUniform: particleCount)
+  let randomTurn = middleDistribution.mask { $0 .< 0.1 }
+    // * TensorR1<Float>(middleMask)
+//  let turn = TensorR1<Float>(highValues - 1) * TensorR1<Float>(1 - middleMask) + randomTurn
+//  headings += (turn * moveAngle)
+//  positions += angleToVector(headings) * moveStep
+
+  /*
   
   // Deposit
   // TODO: This wrapping around negative values needs to be fixed.
@@ -110,4 +127,22 @@ print("Total calculation time: \(String(format: "%.4f", Date().timeIntervalSince
 
 if captureImage {
   try steps.saveAnimatedImage(directory: "output", name: "physarum", delay: 1)
+}
+
+// TODO: argmin and argmax should take in comparable values and provide indices out.
+// TODO: argmin and argmax shoulf provide a squeezing axis parameter.
+@inlinable public func argmin<S,E>(
+    _ lhs: Tensor<S,E>
+) -> Tensor<S,E> where E.Value: Comparable & ComparableLimits {
+  var result = Tensor(like: lhs)
+  currentQueue.argmin(lhs, &result)
+  return result
+}
+
+@inlinable public func argmax<S,E>(
+    _ lhs: Tensor<S,E>
+) -> Tensor<S,E> where E.Value: Comparable & ComparableLimits {
+  var result = Tensor(like: lhs)
+  currentQueue.argmax(lhs, &result)
+  return result
 }
